73a814abd89bfc132563b697c85c796d03c59741
Noor Fatima <noor2239064@gmail.com>
2026-07-07 23:02:18 +0500

    fetched API

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+const url="http://localhost:5000/getProducts"
 function Products() {
+    const [ api, setApi ] = useState([]);
+     async function fetchApi(){
+        const data = await fetch(url);
+        const res = await data.json();
+        console.log(res);
+        setApi(res);
+    }
+
+    useEffect(()=>{
+        fetchApi();
+    },[])
+
   return (
-    <div className='wrapper'>
-      Products
+    <>
+    <div className='wrapper text-center text-4xl font-bold my-4'>
+      Fetch API
     </div>
+    <div className="fetching wrapper">
+
+    </div>
+    </>
   )
 }
 
