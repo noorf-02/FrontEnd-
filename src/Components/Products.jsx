8bac13e40da292bfaf4997477c3a21ea86d5e2c6
Noor Fatima <noor2239064@gmail.com>
2026-07-07 23:22:31 +0500

    added btns

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -19,17 +19,48 @@ function Products() {
       <div className="wrapper text-center text-4xl font-bold my-4">
         Fetch API
       </div>
+      <div className="wrapper filter-btns flex justify-center gap-3">
+        <button
+          onClick={filterCategory("all")}
+          className="bg-green-600 text-white font-medium py-1 px-3 rounded-2xl hover:bg-green-700"
+        >
+          All
+        </button>
+        <button
+          onClick={filterCategory("electronics")}
+          className="bg-green-600 text-white font-medium py-1 px-3 rounded-2xl hover:bg-green-700"
+        >
+          Electronics
+        </button>
+        <button
+          onClick={filterCategory("stationary")}
+          className="bg-green-600 text-white font-medium py-1 px-3 rounded-2xl hover:bg-green-700"
+        >
+          Stationary
+        </button>
+      </div>
       <div className="fetching wrapper flex flex-wrap justify-center p-5 gap-10">
         {api.map((myProduct) => {
           console.log(myProduct);
           return (
-            <div className="card flex flex-col w-[340px] p-4 border-1 rounded-3xl border-gray-200 shadow-2xl gap-2" key={myProduct.id}>
-              <img className="w-[120px] h-[120px] object-cover block m-auto" src={myProduct.image} alt={myProduct.title} />
+            <div
+              className="card flex flex-col w-[340px] p-4 border-1 rounded-3xl border-gray-200 shadow-2xl gap-2"
+              key={myProduct.id}
+            >
+              <img
+                className="w-[120px] h-[120px] object-cover block m-auto"
+                src={myProduct.image}
+                alt={myProduct.title}
+              />
               <h1 className="text-2xl font-bold">{myProduct.title}</h1>
               <p className="text-[16px] ">{myProduct.description}</p>
               <div className="price flex justify-between">
-                <p className="bg-green-700 text-white font-medium text-[14x] px-3 py-1 rounded-2xl">{myProduct.price}$</p>
-                <p className="text-[13px] italic text-gray-800">{myProduct.category}</p>
+                <p className="bg-green-700 text-white font-medium text-[14x] px-3 py-1 rounded-2xl">
+                  {myProduct.price}$
+                </p>
+                <p className="text-[13px] italic text-gray-800">
+                  {myProduct.category}
+                </p>
               </div>
             </div>
           );
